93915bdc425768aa43f6432249e66020dea46cb0
Liza Pincsak <lizapincsak@Lizas-MacBook.local>
2020-10-16 12:47:11 -0500

    finished

--- challenges/closure.js
+++ challenges/closure.js
@@ -25,11 +25,6 @@ myFunction();
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
 function summation(param1){
-  let count = 0;
-  for(let i = 0; i < param1.length; i++)
-{
-    count = count + param1[i];
-}
+  return sumTo(param1)
 }
 
-//reduce? but only one parameter
